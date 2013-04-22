107965bf5ab51b4d895df0a49445b6943c20dbf0
Jeroen <jeroen@jeroen-ubuntu.(none)>
2013-04-22 12:14:22 -0700

    ohmage 2.15 final

--- compile.sh
+++ compile.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 #
 # dependencies
-# sudo apt-get install git ant openjdk-6-jdk openjdk-6-jre unzip git
+# sudo apt-get install git ant openjdk-6-jdk openjdk-6-jre unzip
 
+# Download server source
+rm -Rf awsserver cens-ohmageServer*
+wget https://github.com/cens/ohmageServer/zipball/master -O ohserver.zip
+unzip ohserver.zip
+rm ohserver.zip
+mv cens-ohmageServer* ohserver
 
-### create dir that will contain .war files
+# Download frontend source
+rm -Rf ohfrontend cens-ohmageFrontEnd*
+wget https://github.com/cens/ohmageFrontEnd/zipball/master -O ohfrontend.zip
+unzip ohfrontend.zip
+rm ohfrontend.zip
+mv cens-ohmageFrontEnd* ohfrontend
+
+#hack to change log file location
+sed -i 's/\/opt\/ohmage\/logs\/ohmage.log/\/var\/log\/ohmage\/ohmage.log/g' ohserver/web/WEB-INF/classes/log4j.properties
+
+#create dir that will contain .war files
 mkdir -p warfiles
 
-
-### Compile server:
-git clone git://github.com/cens/ohmageServer.git -b ohmage-2.14 
-cd ohmageServer
-sed -i 's/\/opt\/ohmage\/logs\/ohmage.log/\/var\/log\/ohmage\/ohmage.log/g' web/WEB-INF/classes/log4j.properties
+#compile server code
+cd ohserver
+#ant clean dist
+#mv ./dist/*.war ../warfiles/
 ant clean dist-no_ssl
 mv ./dist/*.war ../warfiles/
 cd ..
-rm -Rf ohmageServer
+rm -Rf ohserver
 
-
-### Compile front-end
-git clone git://github.com/cens/ohmageFrontEnd.git -b master
-cd ohmageFrontEnd
+#compile frontend
+cd ohfrontend
+#ant clean build buildwar
 ant clean build buildwar-nossl
 cp ./*.war ../warfiles/
 cd ..
-rm -Rf ohmageFrontEnd
+rm -Rf ohfrontend
 
